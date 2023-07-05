76d02c2f25777dfe55050d7ebe33c22b774b571b
Ferdinand Malcher <ferdinand@malcher.media>
2023-07-05 11:42:13 +0200

    Interface Book und Buchdaten im Dashboard

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,5 +10,43 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  books: Book[] = [];
 
+  constructor() {
+    this.books = [
+      {
+        isbn: '123',
+        title: 'Angular',
+        description: 'Grundlagen und mehr',
+        rating: 5,
+        price: 42.9
+      },
+      {
+        isbn: '456',
+        title: 'Vue.js',
+        description: 'Das grüne Framework',
+        rating: 3,
+        price: 34.9
+      }
+    ];
+  }
 }
+
+
+/* TODO
+- Datenmodell: Klasse / Interface
+- Daten: statisch, HTTP, …
+- Anzeige im Template
+
+*/
+
+
+
+
+
+
+
+
+
+
+
