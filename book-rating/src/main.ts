58f9e1d44d33c930938084daf5782d22d66cb371
Ferdinand Malcher <ferdinand@malcher.media>
2023-07-05 10:45:13 +0200

    Dashboard und imports und Styles

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -4,34 +4,3 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-
-
-//////////
-
-
-export class Customer {
-
-  private foo = 'fghkfdhbgfd';
-  /*private id: number;
-
-  constructor(id: number) {
-    this.id = id;
-  }*/
-
-
-  constructor(private id: number) {}
-
-  fooBar(arg: number): string {
-
-    setTimeout(() => {
-      console.log('Die ID ist:', this.id);
-    }, 2000);
-
-    return '';
-  }
-}
-
-
-
-const myCustomer = new Customer(3);
-myCustomer.fooBar(5);
