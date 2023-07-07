10edf0ac9062dbfd6050df9a4250900943f72c5c
Ferdinand Malcher <ferdinand@malcher.media>
2023-07-07 14:20:18 +0200

    Routing
    ng g c books/book-details

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { booksRoutes } from './books/books.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // bei Weiterleitung vom leeren Pfad: pathMatch:full
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  ...booksRoutes,
+];
