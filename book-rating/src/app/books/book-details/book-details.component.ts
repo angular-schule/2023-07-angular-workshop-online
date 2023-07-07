3b7c5952980264fa78536d8c895bc8dd194f29df
Ferdinand Malcher <ferdinand@malcher.media>
2023-07-07 15:02:10 +0200

    Routing: Parameter

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
 export class BookDetailsComponent {
+  constructor(private route: ActivatedRoute) {
+    // PULL
+    // const isbn = this.route.snapshot.paramMap.get('isbn'); // path: 'books/:isbn'
+    // console.log(isbn);
 
+    // PUSH
+    this.route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn');
+      console.log(isbn);
+    });
+  }
 }
