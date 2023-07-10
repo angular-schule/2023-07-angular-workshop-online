c453116d5f1ad214a556e32e5eae18fb506e83b7
Ferdinand Malcher <ferdinand@malcher.media>
2023-07-10 15:28:35 +0200

    Reactive Forms: Feedback

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create',
@@ -14,20 +14,73 @@ export class BookCreateComponent {
 
   bookForm = new FormGroup({
     isbn: new FormControl('', {
-      nonNullable: true
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13),
+        Validators.pattern(/^[0-9]*$/)
+      ]
     }),
     title: new FormControl('', {
-      nonNullable: true
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.maxLength(100)
+      ]
     }),
     description: new FormControl('', {
       nonNullable: true
     }),
     rating: new FormControl(1, {
-      nonNullable: true
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(5)
+      ]
     }),
     price: new FormControl(0, {
-      nonNullable: true
-    }),
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(0)
+      ]
+    })
   });
 
+  isInvalid(controlName: keyof typeof this.bookForm.controls): boolean {
+    const control = this.bookForm.controls[controlName];
+    // const control = this.bookForm.get(controlName);
+
+    return control.touched && control.invalid;
+  }
+
+  // Aufgabe: Diese Methode implementieren :-)
+  hasError(controlName: keyof typeof this.bookForm.controls, errorCode: string): boolean {
+    const control = this.bookForm.controls[controlName];
+
+    // return (control.errors?.[errorCode]) && control.touched;
+    // return !!control.getError(errorCode) && control.touched;
+    return control.hasError(errorCode) && control.touched;
+
+  }
+
 }
+
+
+/*
+TODO
+- Validierung
+- Fehlermeldungen
+  - "Die ISBN ist ungültig."
+  - "Die ISBN ist zu kurz."
+- Submit-Button
+- abschicken
+- HTTP
+- bei Erfolg:
+  - Meldung anzeigen
+  - wegnavigieren, z. B. zur Detailseite
+  - Formular zurücksetzen
+
+*/
